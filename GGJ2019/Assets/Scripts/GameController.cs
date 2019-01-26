6a320861cf5b8b9d2c3a4b86a581e31144c53c66
Rienzi Gokea <RGokea@gmail.com>
2019-01-26 11:13:54 -0500

    Started Tile Work

--- GGJ2019/Assets/Scripts/GameController.cs
+++ GGJ2019/Assets/Scripts/GameController.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
+
+	public static GameController controller;
+
+
+	public GameObject tile;
+
+	public TileController[,] tr;
+
+
+
+	void Awake() {
+		if (GameController.controller == null) {
+			GameController.controller = this;
+		}
+		else {
+			// Destroy if it exists already.
+			// this.gameObject;
+		}
+	}
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+    	
+
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
---
+++ GGJ2019/Assets/Scripts/TileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+    
+	public bool visibile = true;
+
+	private SpriteRenderer sr;
+
+	// Start is called before the first frame update
+    void Start() {
+		sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
