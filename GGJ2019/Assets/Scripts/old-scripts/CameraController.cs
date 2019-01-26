c66906b048701e07ce130d226affe198e05444e2
Rienzi Gokea <RGokea@gmail.com>
2019-01-26 10:36:34 -0500

    Added Old NZ Scripts

---
+++ GGJ2019/Assets/Scripts/old-scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.mouseScrollDelta.y > 0) {
+			cam.orthographicSize++;
+		}
+		else if (Input.mouseScrollDelta.y < 0 && cam.orthographicSize > 1) {
+			cam.orthographicSize--;
+		}
+
+
+		if (!UIController.EnteringInfo) {
+
+			if (Input.GetKeyDown("w")) {
+				transform.position = new Vector3(transform.position.x, transform.position.y + 1, -10);
+			} 
+			if (Input.GetKeyDown("a")) {
+				transform.position = new Vector3(transform.position.x - 1, transform.position.y, -10);
+			}
+			if (Input.GetKeyDown("s")) {
+				transform.position = new Vector3(transform.position.x, transform.position.y - 1, -10);
+			}
+			if (Input.GetKeyDown("d")) {
+				transform.position = new Vector3(transform.position.x + 1, transform.position.y, -10);
+			}
+
+		}
+
+
+	}
+}
