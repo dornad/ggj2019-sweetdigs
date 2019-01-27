a7854880ce92693eaf87f500f0dbef2aa411ab9d
Konstantin <kon.boyko@gmail.com>
2019-01-27 14:51:18 -0500

    maybe 3d item rotation

--- GGJ2019/Assets/Scripts/RotationMovement.cs
+++ GGJ2019/Assets/Scripts/RotationMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotationMovement : MonoBehaviour
 {
+    public float rotationSpeed = 5f;
+    private float rotationCurrent = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class RotationMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rotationCurrent += rotationSpeed;
+        transform.Rotate(new Vector3(1, 1, 1), rotationCurrent);
     }
 }
